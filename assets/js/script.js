45a6a00433780e0976757e1fad3960f9d83a58a6
Fatih Rizqon <fatihrizqon@gmail.com>
2022-05-09 11:53:27 +0700

    Enable CORS

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ close.addEventListener("click", function () {
 window.setInterval(
   (window.onload = function getQuote() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://cors.io/?https://zenquotes.io/api/random", true);
+    xhr.open("GET", "https://zenquotes.io/api/random", true);
 
     xhr.onload = function () {
       if (this.readyState == 4 && this.status == 200) {
